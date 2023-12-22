cb60dfe8f8d79f87a0ed05a3b5794c5b84cbc397
paress <farez0830@gmail.com>
2023-12-22 09:46:22 +0700

    update color value

--- app/src/main/java/com/farez/gamehub_compose/ui/theme/Color.kt
+++ app/src/main/java/com/farez/gamehub_compose/ui/theme/Color.kt
@@ -10,5 +10,6 @@ val Purple40 = Color(0xFF6650a4)
 val PurpleGrey40 = Color(0xFF625b71)
 val Pink40 = Color(0xFF7D5260)
 
-val biruMuda = Color(0xFF1BBBB1)
-val darker_biru_muda = Color(0xFF008C83)
+val biruMuda = Color(0xFF34dcd1)
+val biruMuda100 = Color(0xFFd7f7f5)
+val darker_biru_muda = Color(0xFF00ad98)
